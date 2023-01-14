54d014752f8507a40f0465e1ed6ce5719a5d3654
Nicholas Underwood <nachos.underwood@gmail.com>
2023-01-13 18:21:06 -0800

    color change + formatting

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let textContent = $('#text-content');
-let textHeader = $('#text-header h1');
-let textDate = $('#text-header span');
+const textContent = $('#text-content');
+const textHeader = $('#text-header h1');
+const textDate = $('#text-header span');
+const cardHeaders = $('.card-header');
+var lastCard;
 
 function showCard(card){
     textContent.html(card.find(".project-blog").html());
     textHeader.text(card.find(".card-header h2").text());
-    textDate.text(card.find(".card-header p").text())
+    textDate.text(card.find(".card-header p").text());
+
+
+    let h2 = lastCard?.find('h2.grad-text');
+    setTimeout(() => {h2?.removeClass('grad-text');}, 10);
+    $(card.find('.card-header h2')).addClass('grad-text');
+    lastCard = card;
 }
 
 
